a5274214da5aea1a4a3e55e20e34e809e356e30a
Azdren Ymeri <azdreny@gmail.com>
2021-02-21 16:07:45 +0100

    added google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Layout from './Layout/Layout';
 import StoreProvider from './mobx/Store';
+import ReactGA from 'react-ga';
+
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-FNW1CN8YEH");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  },[])
   return (
     <div className="App">
       <StoreProvider>
